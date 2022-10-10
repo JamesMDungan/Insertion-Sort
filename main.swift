92cbd201f1b8cb978651f480a66de83ad055f344
James Dungan <james.dungan@student.allenisd.org>
2022-10-10 08:00:54 -0500

    COmmit

---
+++ main.swift
@@ -0,0 +1,33 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [5, 1, 4, 2, 8]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+var unsortedAlph: [String] = []
+
+while let input = readLine() {
+    unsortedAlph.append(input)
+}
+// Add your code below:
+func sortArray(array: [String]) {
+    var sorted = array
+    for i in 0..<unsortedAlph.count {
+       
+        var j = i
+        while j > 0 {
+            if sorted[j] < sorted[j-1] {
+                sorted.swapAt(j, j-1)
+                
+            }
+            j -= 1
+        }
+        
+       
+    }
+    
+   
+}
+
+sortArray(array: unsortedAlph)
